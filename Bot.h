087535c8b29b33f7b40049cb549b73b8881c5282
wtinker <will.c.tinker@gmail.com>
2026-03-09 11:11:30 -0700

    created get_move function

--- Bot.h
+++ Bot.h
@@ -20,4 +20,17 @@ class Bot {
 	int get_move(const Board&);
 };
 
+int Bot::get_move(const Board& trueBoard) {
+	calculate_weights(trueBoard);
+	int max_weight = weights[0];
+	int weight_index = 0;
+	for (int i = 1; i < 7; i++) {
+		if (weights[i] > max_weight) {
+			max_weight = weights[i];
+			weigt_index = i;
+		}
+	}
+	return weight_index;
+}
+
 #endif // !BOT
