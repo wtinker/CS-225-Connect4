00b9e3d81d2e984b9c08d9d9073bce1f77d54941
wtinker <will.c.tinker@gmail.com>
2026-03-09 09:59:06 -0700

    board created

---
+++ Board.h
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef BOARD
+#define BOARD
+
+class Board {
+  protected:
+	int board[6][7];
+  public:
+	  Board();
+	  void display_board();
+	  void drop_piece(int column, int id);
+	  void reset_board();
+};
+
+Board::Board() {
+	reset_board();
+}
+
+void Board::display_board() {
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 7; j++) {
+			std::cout << board[i][j] << " ";
+		}
+		std::cout << endl;
+	}
+}
+
+void Board::drop_piece(int column, int id) {
+	for (int i = 5; i >= 0; i--) {
+		if (board[i][column] == 0) {
+			board[i][column] = id;
+			break;
+		}
+	}
+}
+
+void Board::reset_board() {
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 7; j++) {
+			board[i][j] = 0;
+		}
+	}
+}
+
+#endif // !BOARD
