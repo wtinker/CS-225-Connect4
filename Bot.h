6df569ad03939a483ad981f37da07517997e4e6f
wtinker <will.c.tinker@gmail.com>
2026-03-09 10:40:46 -0700

    started Bot.h

---
+++ Bot.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef BOT
+#define BOT
+using "Board.h";
+
+class Bot {
+  private:
+    int weights[7];
+	Board tempBoard;
+	void calculate_weights(const Board&);
+	int check_down();
+	int check_up();
+	int check_left();
+	int check_right();
+	int check_upper_left();
+	int check_upper_right();
+	int check_lower_left();
+	int check_lower_right();
+  public:
+	int get_move(const Board&);
+};
+
+#endif // !BOT
