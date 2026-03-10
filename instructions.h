7877c9e5e38295cce5593de5c29f2e5f09ffabd4
CooperNelson1 <cooperjnelson126@gmail.com>
2026-03-09 20:35:21 -0700

    Instructions

--- instructions.h
+++ instructions.h
@@ -4,9 +4,24 @@
 using namespace std;
 // Instructions file
 
-cout << "INSTRUCTIONS GO HERE" << endl;
-
-
-
+void printInstructions() {
+    cout << "================ CONNECT 4 ================" << endl << endl;
+    cout << "Welcome to Connect 4!" << endl;
+    cout << "You will be playing against the computer." << endl << endl;
+    cout << "YOUR GOAL:" << endl;
+    cout << "Be the first player to connect 4 of your pieces in a row." << endl;
+    cout << "You can connect pieces: Horizontally, Vertically, or Diagonally" << endl << endl;
+    cout << "HOW TO PLAY:" << endl;
+    cout << "1. The board has 7 columns and 6 rows." << endl;
+    cout << "2. On your turn, enter the column number where you want to drop your piece and then," << endl
+         << "   your piece will fall to the lowest available space in that column." << endl;
+    cout << "3. The computer will then take its turn." << endl;
+    cout << "4. The game ends when someone connects 4 pieces or the board is full." << endl << endl;
+    cout << "RULES:" << endl;
+    cout << "1. You cannot place a piece in a full column." << endl;
+    cout << "2. If you enter an invalid column, you must choose again." << endl << endl;
+    cout << "Good luck!!" << endl;
+    cout << "===========================================" << endl;
+}
 
 #endif // !INSTRUCTIONS
--- main.cpp
+++ main.cpp
@@ -9,24 +9,55 @@
 using namespace std;
 
 int main() {
+	srand(time(NULL));
+	string ifPlay;
+	string playerName;
+	bool playerFirst = false;
+
 	// rules/instruction
-	
+	printInstructions();
+
 	// ask player if they want to play
+	cout << "Would you like to play (yes or no)? ";
+	cin >> ifPlay;
 
-	// ask play name
+	if(ifPlay == "yes") {
+		while(ifPlay == "yes") {
+			// ask play name
+			cout << "What is your name? ";
+			cin >> playerName;
 
-	// coin flip who goes first
+			// coin flip who goes first
+			cout << "Flipping coin to decide who goes first..." << endl;
+			int coinFace = rand() % 2 + 1;
+			if (coinFace == 1) {
+				playerFirst = false;
+			} else {
+				playerFirst = true;
+			}
 
-	// display matrix(board) and ask player where they want to drop
+			// Game play first move to win/loss/tie
+			// I think this should be a header file to not over croud main
 
-	// let computer go
-	// continue
+			// write to a file with all game details
+			
 
-	// write to a file with all game details
+			// display win vs loss vs tie message (0 = loss, 1 = win, 2 = tie)
+			if(playerWin == 0) {
+				cout << "You have lost. You'll get it next time." << endl;
+			} else if(playerWin == 1) { 
+				cout << "You have WON!! Congratulations!" << endl;
+			} else {
+				cout << "You and the computer have tied. Good game." << endl;
+			}
 
-	// display win vs loss message
-
-	// ask player if they want to play again or quit
+			// ask player if they want to play again or quit
+			cout << "Would you like to play again (yes or no)? ";
+			cin >> ifPlay;
+		}
+	} else {
+		cout << "Sounds good! I'll be here if you change your mind!" << endl;
+	}
 
 	return 0;
 }
