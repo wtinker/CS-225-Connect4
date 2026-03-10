4205d838627fa2cefe8cf11b4b1f56ea3db1648a
wtinker <will.c.tinker@gmail.com>
2026-03-10 14:41:45 -0700

    updated header files

--- Board.h
+++ Board.h
@@ -11,4 +11,15 @@ class Board {
 	  void display_board_simple();
 	  void drop_piece(int column, int id);
 	  void reset_board();
+	  bool check_player_win();
+	  bool check_bot_win();
+	  bool check_full();
+	  int check_down(int id);
+	  int check_up(int id);
+	  int check_left(int id);
+	  int check_right(int id);
+	  int check_upper_left(int id);
+	  int check_upper_right(int id);
+	  int check_lower_left(int id);
+	  int check_lower_right(int id);
 };
--- Bot.h
+++ Bot.h
@@ -8,14 +8,7 @@ class Bot {
     int weights[7];
 	Board tempBoard;
 	void calculate_weights(const Board&);
-	int check_down();
-	int check_up();
-	int check_left();
-	int check_right();
-	int check_upper_left();
-	int check_upper_right();
-	int check_lower_left();
-	int check_lower_right();
   public:
 	int get_move(const Board&);
+	int first_move();
 };
