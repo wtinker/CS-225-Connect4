2809132d3cd51731474b72641d423ee6e734860f
wtinker <will.c.tinker@gmail.com>
2026-03-23 11:49:10 -0700

    migrate some check functions to private

--- Board.h
+++ Board.h
@@ -11,22 +11,22 @@
 class Board {
   private:
 	int board[ROWS][COLUMNS];
+	int check_down(int id, int row, int column);
+	int check_up(int id, int row, int column);
+	int check_left(int id, int row, int column);
+	int check_right(int id, int row, int column);
+	int check_upper_left(int id, int row, int column);
+	int check_upper_right(int id, int row, int column);
+	int check_lower_left(int id, int row, int column);
+	int check_lower_right(int id, int row, int column);
   public:
-	  Board();
-	  void display_board_simple();
-	  void drop_piece(int column, int id);
-	  int get_id(int row, int column);
-	  void reset_board();
-	  bool check_player_win();
-	  bool check_bot_win();
-	  bool check_full();
-	  int check_down(int id, int row, int column);
-	  int check_up(int id, int row, int column);
-	  int check_left(int id, int row, int column);
-	  int check_right(int id, int row, int column);
-	  int check_upper_left(int id, int row, int column);
-	  int check_upper_right(int id, int row, int column);
-	  int check_lower_left(int id, int row, int column);
-	  int check_lower_right(int id, int row, int column);
-	  int check_all_connections(int id);
+	Board();
+	void display_board_simple();
+	void drop_piece(int column, int id);
+	int get_id(int row, int column);
+	void reset_board();
+	bool check_player_win();
+	bool check_bot_win();
+	bool check_full();
+	int check_all_connections(int id);
 };
